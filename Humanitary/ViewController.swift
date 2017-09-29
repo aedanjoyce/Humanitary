34c03abf2b515df90615a27f55472683d8f1615c
Aedan Joyce <zimberfrizz@yahoo.com>
2017-09-29 10:10:15 -0400

    Initial Commit

---
+++ Humanitary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Humanitary
+//
+//  Created by Aedan Joyce on 9/29/17.
+//  Copyright © 2017 Aedan Joyce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
